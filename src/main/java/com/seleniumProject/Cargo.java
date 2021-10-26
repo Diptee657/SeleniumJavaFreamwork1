4d39be73c738095881e8b1bb269c21165394ec61
DSingh <DSingh@INHYDLDSINGH2.logisticsdev.intra>
2021-10-26 19:03:21 +0530

    Added new class - Cargo

--- src/main/java/com/seleniumProject/Cargo.java
+++ src/main/java/com/seleniumProject/Cargo.java
@@ -1,7 +1,6 @@
 package com.seleniumProject;
 
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -9,20 +8,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class OpenIE {
+public class Cargo {
 	public static WebDriver driver;
 
+	
+	
 	public static void main(String[] args) throws InterruptedException {
 
 		navigateToURL();
 		login("nas.csa", "fx5test");
 		
-		driver.findElement(By.cssSelector("td.menuNavigateIcon")).click();
+		WebElement ele=driver.findElement(By.cssSelector("td.menuNavigateIcon"));
+		ele.click();
 		
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("//div[@id='mainMenuItemVertical_Manifest']//a[text()='Cargo']")).click();
+		WebDriverWait wait = new WebDriverWait(driver,30);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Cargo')]")));
+		
+//		driver.findElement(By.cssSelector("div.mainMenuItemVertical_Manifest a[onclick*=\"SiteMenu_onclick('Manifest'\"]")).click();
+//		driver.findElement(By.xpath("//div[@id='mainMenuItemVertical_Manifest']//a[onclick*=\"SiteMenu_onclick('Manifest'\"]")).click();
+		driver.findElement(By.xpath("//div[@id='mainMenuItemVertical_Manifest']/a")).click();
 
 
 //		
@@ -39,13 +46,14 @@ public class OpenIE {
 		
 	}
 
-	public static void navigateToURL() {
+	public static void navigateToURL() throws InterruptedException {
 		System.setProperty("webdriver.ie.driver", "C:\\Drivers\\IEDriverServer.exe");
 		driver = new InternetExplorerDriver();
 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+		Thread.sleep(3000);
 		driver.get("http://10.138.108.44/MCKWFX5TEST/Main.aspx");
 		System.out.println(driver.getTitle());
 
@@ -72,8 +80,9 @@ public class OpenIE {
 		driver.findElement(By.cssSelector("input#sUserPassword")).sendKeys(Password);
 
 		driver.findElement(By.cssSelector("input.FX50loginPanelLoginButton")).click();
-		driver.switchTo().alert().accept();
-		driver.switchTo().alert().accept();
+//		driver.switchTo().alert().dismiss();
+//		driver.switchTo().alert().dismiss();
 
 	}
+
 }
